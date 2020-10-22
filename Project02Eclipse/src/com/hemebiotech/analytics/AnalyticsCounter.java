3a936029e5fce097492ce622ce2449d1e1d69295
Decarte Adam <mousa-06@hotmail.com>
2020-10-22 19:20:34 +0200

    Refactoring of Main class

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,30 +1,39 @@
 package com.hemebiotech.analytics;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 
 public class AnalyticsCounter {
-	
+	Map<String, Integer> result;
+	ArrayList<String> list;
+
 	public static void main(String args[]) throws Exception {
 
-		Map<String, Integer> result;
-		ArrayList<String> list;
+		AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+		analyticsCounter.readSymptoms();
+		analyticsCounter.calculSymptoms();
+		analyticsCounter.generateSymptoms();
+	}
 
-		/* Read the 'symptoms.txt file filepath is a path to the file t read */
-		ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile("/Users/adam/Documents/OC_Projects/Project_2/Project02Eclipse/symptoms.txt");
-		list = (ArrayList<String>) readSymptomDataFromFile.getSymptoms();
+	private void generateSymptoms() throws IOException {
+		/* Generate a new 'result.out' output file
+		 result is map of counted symptoms */
+		GenerateSymptomsDataFile generateSymptomsDataFile = new GenerateSymptomsDataFile(result);
+		generateSymptomsDataFile.generate();
+	}
 
-
+	private void calculSymptoms() {
 		/* Compute symptoms in 'symptoms.txt' file
 		  list is an arrayList of symptoms */
 		CalculSymptoms calculSymptoms = new CalculSymptoms(list);
 		result =  calculSymptoms.calculSymptoms();
 		System.out.println(result);
+	}
 
-		/* Generate a new 'result.out' output file
-		 result is map of counted symptoms */
-		GenerateSymptomsDataFile generateSymptomsDataFile = new GenerateSymptomsDataFile(result);
-		generateSymptomsDataFile.generate();
-
+	public void readSymptoms(){
+		/* Read the 'symptoms.txt file filepath is a path to the file t read */
+		ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile("/Users/adam/Documents/OC_Projects/Project_2/Project02Eclipse/symptoms.txt");
+		list = (ArrayList<String>) readSymptomDataFromFile.getSymptoms();
 	}
 }
